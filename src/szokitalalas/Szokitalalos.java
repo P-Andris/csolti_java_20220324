5ad0d4adf78c809c593e209d8e5b05cd140aade3
P-Andris <pillerandras0714@gmail.com>
2022-03-24 12:22:07 +0100

    kivalaszt, tip_be function

--- src/szokitalalas/Szokitalalos.java
+++ src/szokitalalas/Szokitalalos.java
@@ -5,22 +5,25 @@ import java.util.Scanner;
 
 public class Szokitalalos {
 
-    Random r = new Random();
-    Scanner sc = new Scanner(System.in);
+    public static Random r = new Random();
+    public static Scanner sc = new Scanner(System.in);
 
     public static String kivalaszt() {
         String[] szavak = {"tó", "ló", "só", "kő", "él", "öl", "fa", "ez", "íz", "az", "őz", "te", "tű", "ül", "fű", "én", "ön", "uj", "új", "éj", "ék", "ét", "nő", "ti", "ki", "sí", "ér"};
-        return szavak[0];
+        int index = r.nextInt(szavak.length);
+        return szavak[index];
     }
 
     public static String tip_be() {
-        String tip = "";
+        System.out.print("Adj meg egy két betűs szót: ");
+        String tip = sc.nextLine();
         return tip;
     }
 
     public static void eredmeny(String tip, String szo) {
         
     }
+
     public static void main(String[] args) {
         String szo = kivalaszt();
         String tip;
