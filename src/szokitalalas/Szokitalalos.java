f528666d4eb312a4030a6710daad3bce43fd626f
Kalandor01 <rohovszkyakoska@gmail.com>
2022-03-24 12:47:03 +0100

    encoding error

--- src/szokitalalas/Szokitalalos.java
+++ src/szokitalalas/Szokitalalos.java
@@ -9,7 +9,7 @@ public class Szokitalalos {
     public static Scanner sc = new Scanner(System.in);
 
     public static String kivalaszt() {
-        String[] szavak = {"tó", "ló", "só", "kő", "él", "öl", "fa", "ez", "íz", "az", "őz", "te", "tű", "ül", "fű", "én", "ön", "uj", "új", "éj", "ék", "ét", "nő", "ti", "ki", "sí", "ér"};
+        String[] szavak = {"fa", "ez", "az", "te", "ti", "ki"};
         int index = r.nextInt(szavak.length);
         return szavak[index];
     }
@@ -20,24 +20,30 @@ public class Szokitalalos {
         return tip;
     }
 
-    public static void eredmeny(String tip, String szo) {
-        if(tip == szo)
-            System.out.println("Kitaláltad!");
+    public static int eredmeny(String tip, String szo, int t_num) {
+        t_num++;
+        if(tip.length() != 2)
+            System.out.println("Nem jó szó.");
+        else if(tip.charAt(0) == szo.charAt(0) && tip.charAt(1) == szo.charAt(1))
+            System.out.println(t_num + " próbálkozásból kitaláltad!");
         else if(tip.charAt(0) == szo.charAt(0) || tip.charAt(1) == szo.charAt(1))
             System.out.println("Az egyik betű jó, és jó helyen van.");
         else if(tip.charAt(0) == szo.charAt(1) || tip.charAt(1) == szo.charAt(0))
             System.out.println("Az egyik betű jó, de nem jó helyen van.");
         else
             System.out.println("Nem jó szó.");
+        return t_num;
     }
 
     public static void main(String[] args) {
         String szo = kivalaszt();
         String tip;
+        int t_num = 0;
         do {
             tip = tip_be();
-            eredmeny(tip, szo);
-        } while(tip != szo);
+            t_num = eredmeny(tip, szo, t_num);
+            //System.out.println(szo + " " + tip);
+        } while(tip.length() != 2 || tip.charAt(0) != szo.charAt(0) || tip.charAt(1) != szo.charAt(1));
 
     }
     
